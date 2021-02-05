f205cb9e7d1d7d417b97e6c9785d4460f9b66325
Karlan Astrego <karlanastrego@MacBook-Pro-van-Karlan.local>
2021-02-05 18:23:01 +0100

    cred 21

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,26 @@ require('dotenv').config({
 module.exports = {
   siteMetadata: {
     title: 'Soundsfromtheweb',
-    siteUrl: 'https://needstobetheproductionurl.com',
+    siteUrl: 'https://soundsfromtheweb.com',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        allowList: [
+          'FIREBASE_TYPE',
+          'FIREBASE_PROJECT_ID',
+          'FIREBASE_PRIVATE_KEY_ID',
+          'FIREBASE_PRIVATE_KEY',
+          'FIREBASE_CLIENT_EMAIL',
+          'FIREBASE_CLIENT_ID',
+          'FIREBASE_AUTH_URI',
+          'FIREBASE_TOKEN_URI',
+          'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
+          'FIREBASE_CLIENT_X509_CERT_URL',
+        ],
+      },
+    },
     {
       resolve: 'gatsby-firesource',
       options: {
